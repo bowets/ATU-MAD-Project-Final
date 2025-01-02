1cc1d5aca669d67ee628bd93136c306f174bcb99
Borna Forsek <bforsek@gmail.com>
2025-01-02 23:22:32 +0100

    create my-http service

---
+++ G00425715/src/app/services/my-http.service.ts
@@ -0,0 +1,15 @@
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CapacitorHttp, HttpOptions } from '@capacitor/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyHttpService {
+
+  constructor() { }
+
+  async get(options: HttpOptions) {
+    return await CapacitorHttp.get(options);
+  }
+}
