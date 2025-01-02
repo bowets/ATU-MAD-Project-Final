4934538b797fa6739f8e38e41146b5acfadf816c
Borna Forsek <bforsek@gmail.com>
2025-01-02 14:38:24 +0100

    add settings icon to home page toolbar

--- G00425715/src/app/home/home.page.ts
+++ G00425715/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonButtons, 
+  IonButton, 
+  IonIcon } 
+  from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { settingsOutline} from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      'settings-outline': settingsOutline
+    });
+  }
+
 }
