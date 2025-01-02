1c850a55f2bc9b679542c28eb384f6ca740636a3
Borna Forsek <bforsek@gmail.com>
2025-01-02 21:20:19 +0100

    set country to storage when click search

--- G00425715/src/app/home/home.page.ts
+++ G00425715/src/app/home/home.page.ts
@@ -13,6 +13,8 @@ import { addIcons } from 'ionicons';
 import { settingsOutline} from 'ionicons/icons';
 import { RouterModule } from '@angular/router';
 import { MyDataService } from '../services/my-data.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -28,20 +30,33 @@ import { MyDataService } from '../services/my-data.service';
     IonButton, 
     IonIcon, 
     IonInput,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
 })
 export class HomePage {
-  constructor(private ds: MyDataService) {
+
+  // ------ VARIABLES ------------
+
+  countryInput: string = "";
+
+
+
+  // ------ CONSTRUCTOR ------------
+  constructor(private ds: MyDataService, private router: Router) {
     addIcons({
       'settings-outline': settingsOutline
     });
   }
 
+  // ------- LIFECYCLE ------------
+
   // On initialization, check if settings exist and set default if not
   ngOnInit() {
     this.checkSettings();
   }
+
+  // ------- METHODS ------------
 
   // Check if settings exist. If not, set metric as default
   async checkSettings() {
@@ -51,10 +66,20 @@ export class HomePage {
     if (settings) {
       console.log("Settings exist: " + settings);
       // If settings do not exist (the app is being run for the first time), set metric as default
-  } else {
-    console.log("Settings do not exist. Setting default settings: metric");
-    this.ds.set('settings', 'metric');
+    } else {
+      console.log("Settings do not exist. Setting default settings: metric");
+      this.ds.set('settings', 'metric');
+    }
   }
 
-}
+  async searchCountry() {
+    if (!this.countryInput) {
+      console.log("No country entered");
+      return;
+    }
+    console.log("Searching for country " + this.countryInput);
+    await this.ds.set('country', this.countryInput);
+    this.router.navigate(['/countries']);
+  }
+
 }
